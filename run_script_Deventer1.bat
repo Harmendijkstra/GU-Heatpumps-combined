3f592272657477f558c4afc201bed0238168aacf
your.user.name <Harmen.Dijkstra@dnv.com>
2024-10-17 09:52:59 +0200

    Change bat file such that it closes afterwards

--- run_script_Deventer1.bat
+++ run_script_Deventer1.bat
@@ -4,6 +4,3 @@ call conda activate GU-Heatpumps
 
 REM Run the Python script with the provided arguments
 python HPimport.py "Meetset1-Deventer" "Deventer"
-
-REM Optional: Pause to keep the command window open after execution
-pause
